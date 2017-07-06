af904d52ce8a22c158ddafdf06f96a9ec6658a92
chenwenbo <594017622@qq.com>
2017-07-06 20:47:00 +0800

    spring data rest 添加

---
+++ src/main/java/com/boldseas/demo/repository/AddressRepository.java
@@ -0,0 +1,22 @@
+package com.boldseas.demo.repository;
+
+import com.boldseas.demo.domain.Address;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
+
+import java.util.List;
+
+/**
+ * description :
+ * author : wenbo.chen@boldseas.com
+ * Created time : 2017/07/06 20:02.
+ */
+@RepositoryRestResource(path = "address")
+public interface AddressRepository extends JpaRepository<Address, Long>{
+
+    @RestResource(path = "findByCity")
+    List<Address> findByCityStartingWith(@Param("city") String city);
+
+}
