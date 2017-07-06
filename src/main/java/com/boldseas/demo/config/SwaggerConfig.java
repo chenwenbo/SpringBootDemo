e83e3fedb129e3ee9ec1d77100b95976dee83a6a
chenwenbo <594017622@qq.com>
2017-07-06 19:38:34 +0800

    swagger 重构

--- src/main/java/com/boldseas/demo/config/SwaggerConfig.java
+++ src/main/java/com/boldseas/demo/config/SwaggerConfig.java
@@ -2,13 +2,14 @@ package com.boldseas.demo.config;
 
 import org.springframework.beans.factory.annotation.Configurable;
 import org.springframework.context.annotation.Bean;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static springfox.documentation.builders.PathSelectors.regex;
 
 /**
  * description :
@@ -19,26 +20,28 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @EnableSwagger2
 public class SwaggerConfig {
 
+
     @Bean
-    public Docket createRestAPI(){
+    public Docket productApi() {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.boldseas"))
-                .paths(PathSelectors.any())
+                .paths(regex("/api/.*"))
                 .build()
-                .apiInfo(instance());
+                .apiInfo(metaData());
     }
 
-    private ApiInfo instance() {
-        return new ApiInfo(
-                "DEMO REST API",
-                "USER REST API",
+    private ApiInfo metaData() {
+        ApiInfo apiInfo = new ApiInfo(
+                "SpringBoot REST API",
+                "SpringBoot REST API",
                 "1.0",
                 "Terms of service",
-                new Contact("尔东陈", "https://github.com/chenwenbo", "berry@gmail.com"),
+                new Contact("chenwenbo", "https://github.com/chenwenbo", "beearys@gmail.com"),
                 "Apache License Version 2.0",
                 "https://www.apache.org/licenses/LICENSE-2.0");
-
+        return apiInfo;
     }
+
 
 }
